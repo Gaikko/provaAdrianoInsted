3868194a2d3599ad00e80d22756609d21e00ba1d
Gaikko <Gaikko@gmail.com>
2022-11-21 22:32:03 -0400

    debug ok

--- src/pages/Weather/components/ApiWeather.js
+++ src/pages/Weather/components/ApiWeather.js
@@ -2,10 +2,9 @@ import React, { useState } from 'react'
 import './ApiWeather.css';
 
 const ApiWeather = ({ ph }) => {
-  const [cidade, setCidade] = useState("");
+  const [cidade, setCidade] = useState();
   const SerachInput = (e) => {
     e.preventDefault();
-    console.log(cidade);
     let appid = "91afb389b6c1f75e125ed8e51d786ada";
     let currentValue = document.querySelector('input[name=serachInput]').value;
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=${appid}&units=metric`;
@@ -13,7 +12,6 @@ const ApiWeather = ({ ph }) => {
       .then(res => res.json())
       .then(data => {
         const { name, main, sys, weather } = data;
-        console.log({ data });
         if (sys !== undefined) {
           if (weather !== undefined) {
             const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
@@ -35,7 +33,6 @@ const ApiWeather = ({ ph }) => {
           setCidade("");
         }
       })
-    console.log(currentValue);
   }
   return (
     <div>
